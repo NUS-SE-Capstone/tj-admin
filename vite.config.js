bc2263bf2ff5cb8a324184c46ec9b9f7f43105dc
whqwq <1776678487@qq.com>
2025-04-13 22:52:28 +0800

    import dependency

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,46 @@
-import { fileURLToPath, URL } from 'node:url'
+// 项目配置页面
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import path from "path";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const CWD = process.cwd();
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+//配置参考 https://vitejs.dev/config/
+export default defineConfig((mode) => {
+  const { VITE_BASE_URL } = loadEnv(mode, CWD);
+  return {
+    base: "./",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [vue(), vueJsx(), svgLoader()],
+    // server:{
+    //   port: 8081
+    // }
+    server: {
+      port: 18081,
+      host: "0.0.0.0",
+      // proxy: {
+      //     '/img-tx': {
+      //     target: 'http://wisehub-1312394356.cos.ap-shanghai.myqcloud.com',
+      //     // rewrite: (path) => {
+      //     //   return path.replace(/^\/img-tx/, '')
+      //     // }
+      //   },
+      // },
+      proxy: {
+        "/img-tx": {
+          target: "https://tjxt-dev.itheima.net",
+          changeOrigin: true,
+          // rewrite: (path) => {
+          //   return path.replace(/^\/img-tx/, '')
+          // }
+        },
+      },
+    },
+  };
+});

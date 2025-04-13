dc6559f10ac794c457d2d2fd585e1bd2728f8bf6
whqwq <1776678487@qq.com>
2025-04-13 23:13:01 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
-import './assets/main.css'
+import { createApp } from "vue";
+// 组件库
+import ElementPlus from "element-plus";
+import '@/style/font-style/iconfont.css';
+import 'element-plus/theme-chalk/index.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+import App from "./App.vue";
+// 全局样式
+import "@/style/index.scss";
+// 权限-路由拦截
+import "./permission";
+// 数据存储、路由
+import { store } from "./store";
+import router from "./router";
+// 防止用户频繁点击按钮
+import preventReClick from "@/utils/preventRepeatClick";
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(store);
+app.use(router);
+app.use(preventReClick);
+app.mount("#app");

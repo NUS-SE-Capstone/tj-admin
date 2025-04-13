427ab1394d812cc9fc4fba9300d04847abbd28d4
whqwq <1776678487@qq.com>
2025-04-13 23:12:17 +0800

    add perm

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,48 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
+<!-- 主页面 -->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+import { defineComponent } from "vue";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+let locale = zhCn;
+import { computed, onMounted } from 'vue';
+// import config from '@/config/style';
+// import { useSettingStore } from '@/store';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+// const store = useSettingStore();
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+// const mode = computed(() => {
+//   console.log(33)
+//   return store.displayMode;
+// });
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+import { getTypeAll } from "@/api/api"
+// 缓存
+import { catchDataesStore } from '@/store';
+let store = catchDataesStore();
+onMounted(() => {
+  if(!store.getCategoryTree){
+     getTypeAll({admin:true})
+        .then((res) => {
+          if (res.code === 200) {
+            store.setCategoryTree(res.data);
+            let map = {};
+            res.data.forEach(f => {
+              map[f.id] = {id:f.id, name:f.name}
+              f.children.forEach(s => {
+                map[s.id] = {id:s.id, name:s.name, parentId: s.parentId}
+                s.children.forEach(t => map[t.id] = {id:t.id, name:t.name, parentId:t.parentId})
+              })
+            })
+            store.setCategoryMap(map);
+          }
+        })
+        .catch((err) => console.log(err))
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+});
+</script>
